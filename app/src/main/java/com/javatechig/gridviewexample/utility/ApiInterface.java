36cf26f9a820b69b2cd12f3a49718f89b1bf0f29
Joonhee <pakjoonhee@gmail.com>
2016-11-11 19:48:13 -0800

    feat: new java classes for Api calls for retrofit

---
+++ app/src/main/java/com/javatechig/gridviewexample/utility/ApiInterface.java
@@ -0,0 +1,20 @@
+package com.javatechig.gridviewexample.utility;
+
+import com.javatechig.gridviewexample.data.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by joonheepak on 11/11/16.
+ */
+
+public class ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRateMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
