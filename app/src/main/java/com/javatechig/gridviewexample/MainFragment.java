c4aaa6a54f1f0e50a388d1bfdba4acff05e598cf
Joonhee <pakjoonhee@gmail.com>
2016-11-23 23:47:10 -0800

    chore: removal of some imports and usage of some logs

--- app/src/main/java/com/javatechig/gridviewexample/MainFragment.java
+++ app/src/main/java/com/javatechig/gridviewexample/MainFragment.java
@@ -21,16 +21,7 @@ import com.javatechig.gridviewexample.data.MoviesResponse;
 import com.javatechig.gridviewexample.utility.ApiClient;
 import com.javatechig.gridviewexample.utility.ApiInterface;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
